8da91f6e668bf38002adeeeffc1da08559bf5718
Caio Felix <caiohalcsik@gmail.com>
2024-01-03 17:31:25 -0300

    feat: working in progress

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+export interface CreateLeadParams {
+  email: string;
+  name: string;
+  metadata: Record<string, any>;
+  sessionId: string;
+}
+
+
+export const LeadlifySDK = (apiKey: string) => {
+  return () => ({
+    Leads: {
+      create: async (params: CreateLeadParams) => {
+        const { email, name, metadata, sessionId } = params;
+
+        const requestBody = {
+          email,
+          name,
+          metadata,
+          session_id: sessionId,
+        };
+        const parsedRequestBody = JSON.stringify(requestBody);
+
+        const response = await fetch("https://api.leadlify.com/v1/leads", {
+          body: parsedRequestBody,
+          headers: {
+            'x-api-key': apiKey,
+          },
+        });
+
+		return response.json()
+      },
+    },
+  });
+};
